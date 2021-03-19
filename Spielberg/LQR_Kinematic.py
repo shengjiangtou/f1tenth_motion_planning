396b1521fe27883149bfcd9ea0d19595e5b9d599
Johannes Betz <betz@ftm.mw.tum.de>
2021-03-18 22:03:51 -0400

    WORKING: LQR-Kinematic working

--- Spielberg/LQR_Kinematic.py
+++ Spielberg/LQR_Kinematic.py
@@ -54,104 +54,6 @@ def pi_2_pi(angle):
 
     return angle
 
-class StanleyPlanner:
-    """
-    This is the class for the Front Weeel Feedback Controller (Stanley) for tracking the path of the vehicle
-
-    Ref:
-    - [Stanley: The robot that won the DARPA grand challenge](http://isl.ecst.csuchico.edu/DOCS/darpa2005/DARPA%202005%20Stanley.pdf)
-    - [Autonomous Automobile Path Tracking](https://www.ri.cmu.edu/pub_files/2009/2/Automatic_Steering_Methods_for_Autonomous_Automobile_Path_Tracking.pdf)
-    """
-
-    def __init__(self, conf, wb):
-        self.wheelbase = wb                 # Wheelbase of the vehicle
-        self.conf = conf                    # Current configuration for the gym based on the maps
-        self.load_waypoints(conf)           # Waypoints of the raceline
-        self.max_reacquire = 20.
-
-    def load_waypoints(self, conf):
-        """
-        Loading the x and y waypoints in the "..._raceline.csv" which includes the path to follow
-        """
-
-        self.waypoints = np.loadtxt(conf.wpt_path, delimiter=conf.wpt_delim, skiprows=conf.wpt_rowskip)
-
-    def calc_theta_and_ef(self, vehicle_state, waypoints):
-        """
-        calc theta and ef
-        Theta is the heading of the car, this heading must be minimized
-        ef = crosstrack error/The distance from the optimal path/ lateral distance in frenet frame (front wheel)
-        """
-
-        ############# Calculate closest point to the front axle based on minimum distance calculation ################
-        # Calculate Position of the front axle of the vehicle based on current position
-        fx = vehicle_state[0] + self.wheelbase * math.cos(vehicle_state[2])
-        fy = vehicle_state[1] + self.wheelbase * math.sin(vehicle_state[2])
-        position_front_axle = np.array([fx, fy])
-
-        # Find target index for the correct waypoint by finding the index with the lowest distance value/hypothenuses
-        wpts = np.vstack((self.waypoints[:, self.conf.wpt_xind], self.waypoints[:, self.conf.wpt_yind])).T
-        nearest_point_front, nearest_dist, t, target_index = nearest_point_on_trajectory(position_front_axle, wpts)
-
-        # Calculate the Distances from the front axle to all the waypoints
-        distance_nearest_point_x= fx - nearest_point_front[0]
-        distance_nearest_point_y = fy - nearest_point_front[1]
-        vec_dist_nearest_point = np.array([distance_nearest_point_x, distance_nearest_point_y])
-
-        ###################  Calculate the current Cross-Track Error ef in [m]   ################
-        # Project crosstrack error onto front axle vector
-        front_axle_vec_rot_90 = np.array([[math.cos(vehicle_state[2] - math.pi / 2.0)],
-                                          [math.sin(vehicle_state[2] - math.pi / 2.0)]])
-
-        #vec_target_2_front = np.array([dx[target_index], dy[target_index]])
-
-        # Caculate the cross-track error ef by
-        ef = np.dot(vec_dist_nearest_point.T, front_axle_vec_rot_90)
-
-        #############  Calculate the heading error theta_e  normalized to an angle to [-pi, pi]     ##########
-        # Extract heading for the optimal raceline
-        # BE CAREFUL: If your raceline is based on a different coordinate system you need to -+ pi/2 = 90 degrees
-        theta_raceline = waypoints[target_index][3]
-
-        # Calculate the heading error by taking the difference between current and goal + Normalize the angles
-        theta_e = pi_2_pi(theta_raceline - vehicle_state[2])
-
-        # Calculate the target Veloctiy for the desired state
-        goal_veloctiy = waypoints[target_index][5]
-
-        return theta_e, ef, target_index, goal_veloctiy
-
-    def controller(self, vehicle_state, waypoints, vgain):
-        """
-        Front Wheel Feedback Controller to track the path
-        Based on the heading error theta_e and the crosstrack error ef we calculate the steering angle
-        Returns the optimal steering angle delta is P-Controller with the proportional gain k
-        """
-
-        k_path = 8.2                 # Proportional gain for path control
-        k_veloctiy = vgain           # Proportional gain for speed control, defined globally in the gym
-        theta_e, ef, target_index, goal_veloctiy = self.calc_theta_and_ef(vehicle_state, waypoints)
-
-        # Caculate steering angle based on the cross track error to the front axle in [rad]
-        cte_front = math.atan2(k_path * ef, vehicle_state[3])
-
-        # Calculate final steering angle/ control input in [rad]: Steering Angle based on error + heading error
-        delta = cte_front + theta_e
-
-        # Calculate final speed control input in [m/s]:
-        #speed_diff = k_veloctiy * (goal_veloctiy-velocity)
-        speed = k_veloctiy * goal_veloctiy
-
-        return delta, speed
-
-    def plan(self, pose_x, pose_y, pose_theta, velocity, vgain, lap):
-        #Define a numpy array that includes the current vehicle state: x,y, theta, veloctiy
-        vehicle_state = np.array([pose_x, pose_y, pose_theta, velocity])
-
-        #Calculate the steering angle and the speed in the controller
-        steering_angle, speed = self.controller(vehicle_state, self.waypoints, vgain,lap)
-
-        return speed,steering_angle
 
 class Datalogger:
     """
@@ -264,10 +166,10 @@ class LQR_Kinematic_Planner:
         :return: steering angle (optimal u), theta_e, e_cg
         """
         state_size = 4
-        matrix_q = [0.5, 0.0, 1.0, 0.0]
+        matrix_q = [0.7, 0.0, 1.2, 0.0]
         matrix_r = [1.0]
-        max_iteration = 150
-        eps = 0.01
+        max_iteration = 250
+        eps = 0.001
 
         # Use the timestep of the simulation as a controller input calculation
         ts_ = timestep
@@ -294,25 +196,26 @@ class LQR_Kinematic_Planner:
         matrix_state_[2][0] = theta_e
         matrix_state_[3][0] = (theta_e - theta_e_old) / ts_
 
-        steer_angle_feedback = -(matrix_k_ @ matrix_state_)[0][0]
+        steer_angle_feedback = (matrix_k_ @ matrix_state_)[0][0]
 
         steer_angle_feedforward = self.ComputeFeedForward(k_ref, self.wheelbase)
 
         steer_angle = steer_angle_feedback + steer_angle_feedforward
 
-        return steer_angle, v_ref
+        # Calculate final speed control input in [m/s]:
+        # speed_diff = k_veloctiy * (goal_veloctiy-velocity)
+        speed = v_ref * vgain
+
+        return steer_angle, speed
 
     @staticmethod
-    def ComputeFeedForward(ref_curvature,wb):
+    def ComputeFeedForward(ref_curvature,wheelbase):
         """
         calc feedforward control term to decrease the steady error.
         :param ref_curvature: curvature of the target point in ref trajectory
         :return: feedforward term
         """
-
-        wheelbase_ = wb
-
-        steer_angle_feedforward = wheelbase_ * ref_curvature
+        steer_angle_feedforward = wheelbase * ref_curvature
 
         return steer_angle_feedforward
 
@@ -357,8 +260,7 @@ class LQR_Kinematic_Planner:
             P = P_next
 
         if num_iteration >= max_num_iteration:
-            print("LQR solver cannot converge to a solution",
-                  "last consecutive result diff is: ", diff)
+            print("LQR solver cannot converge to a solution", "last consecutive result diff is: ", diff)
 
         K = np.linalg.inv(BT @ P @ B + R) @ (BT @ P @ A + MT)
 
@@ -427,7 +329,7 @@ if __name__ == '__main__':
 
         obs, step_reward, done, info = env.step(np.array([[steer, speed]]))
         laptime += step_reward
-        env.render(mode='human')
+        env.render(mode='human_fast')
 
         if conf_dict['logging'] == 'True':
             logging.logging(obs['poses_x'][0], obs['poses_y'][0], obs['poses_theta'][0], obs['linear_vels_x'][0], obs['lap_counts'],speed, steer)
