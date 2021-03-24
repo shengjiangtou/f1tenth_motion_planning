9cd8121536e6a4036b55cee44a19d59e306355c2
Johannes Betz <betz@ftm.mw.tum.de>
2021-03-24 17:17:53 -0400

    Integration Calculation for Frenet Paths

--- Spielberg/FrenetPlanner.py
+++ Spielberg/FrenetPlanner.py
@@ -7,6 +7,7 @@ import math
 from numba import njit
 import matplotlib.pyplot as plt
 import pickle
+import copy
 
 """ 
 Planner Helpers
@@ -151,6 +152,111 @@ class Datalogger:
         self.steering_angle.append(control_steering)
         self.lapcounter.append(lap)
 
+class QuarticPolynomial:
+
+    def __init__(self, xs, vxs, axs, vxe, axe, time):
+        # calc coefficient of quartic polynomial
+
+        self.a0 = xs
+        self.a1 = vxs
+        self.a2 = axs / 2.0
+
+        A = np.array([[3 * time ** 2, 4 * time ** 3],
+                      [6 * time, 12 * time ** 2]])
+        b = np.array([vxe - self.a1 - 2 * self.a2 * time,
+                      axe - 2 * self.a2])
+        x = np.linalg.solve(A, b)
+
+        self.a3 = x[0]
+        self.a4 = x[1]
+
+    def calc_point(self, t):
+        xt = self.a0 + self.a1 * t + self.a2 * t ** 2 + \
+             self.a3 * t ** 3 + self.a4 * t ** 4
+
+        return xt
+
+    def calc_first_derivative(self, t):
+        xt = self.a1 + 2 * self.a2 * t + \
+             3 * self.a3 * t ** 2 + 4 * self.a4 * t ** 3
+
+        return xt
+
+    def calc_second_derivative(self, t):
+        xt = 2 * self.a2 + 6 * self.a3 * t + 12 * self.a4 * t ** 2
+
+        return xt
+
+    def calc_third_derivative(self, t):
+        xt = 6 * self.a3 + 24 * self.a4 * t
+
+        return xt
+
+class QuinticPolynomial:
+
+    def __init__(self, xs, vxs, axs, xe, vxe, axe, time):
+        # calc coefficient of quintic polynomial
+        # See jupyter notebook document for derivation of this equation.
+        self.a0 = xs
+        self.a1 = vxs
+        self.a2 = axs / 2.0
+
+        A = np.array([[time ** 3, time ** 4, time ** 5],
+                      [3 * time ** 2, 4 * time ** 3, 5 * time ** 4],
+                      [6 * time, 12 * time ** 2, 20 * time ** 3]])
+        b = np.array([xe - self.a0 - self.a1 * time - self.a2 * time ** 2,
+                      vxe - self.a1 - 2 * self.a2 * time,
+                      axe - 2 * self.a2])
+        x = np.linalg.solve(A, b)
+
+        self.a3 = x[0]
+        self.a4 = x[1]
+        self.a5 = x[2]
+
+    def calc_point(self, t):
+        xt = self.a0 + self.a1 * t + self.a2 * t ** 2 + \
+             self.a3 * t ** 3 + self.a4 * t ** 4 + self.a5 * t ** 5
+
+        return xt
+
+    def calc_first_derivative(self, t):
+        xt = self.a1 + 2 * self.a2 * t + \
+             3 * self.a3 * t ** 2 + 4 * self.a4 * t ** 3 + 5 * self.a5 * t ** 4
+
+        return xt
+
+    def calc_second_derivative(self, t):
+        xt = 2 * self.a2 + 6 * self.a3 * t + 12 * self.a4 * t ** 2 + 20 * self.a5 * t ** 3
+
+        return xt
+
+    def calc_third_derivative(self, t):
+        xt = 6 * self.a3 + 24 * self.a4 * t + 60 * self.a5 * t ** 2
+
+        return xt
+
+class FrenetPath:
+
+    def __init__(self):
+        self.t = []
+        self.d = []
+        self.d_d = []
+        self.d_dd = []
+        self.d_ddd = []
+        self.s = []
+        self.s_d = []
+        self.s_dd = []
+        self.s_ddd = []
+        self.cd = 0.0
+        self.cv = 0.0
+        self.cf = 0.0
+
+        self.x = []
+        self.y = []
+        self.yaw = []
+        self.ds = []
+        self.c = []
+
 class FrenetPlaner:
     """
     Frenet optimal trajectory generator
@@ -291,17 +397,100 @@ class FrenetPlaner:
         speed = v_ref * vgain
 
         return steer_angle, speed
-    def calc_frenet_path (self, speed, c_d, c_d_d, c_d_dd, s0):
-        path = 0
 
-        return path
+    def calc_frenet_paths(self, c_speed, c_d, c_d_d, c_d_dd, s0):
+        # Parameter
+        MAX_ROAD_WIDTH = 1.1/2      # maximum road width [m]
+        D_ROAD_W = 0.1              # road width sampling length [m]
+        MAX_T = 5.0                 # max prediction time [m]
+        MIN_T = 4.0                 # min prediction time [m]
+        DT = 0.1                    # Sampling time in s
+        TARGET_SPEED = 8.0          # Target speed in [m/s]
+        D_T_S = 1.0                 # target speed sampling length [m/s]
+        N_S_SAMPLE = 4              # sampling number of target speed
 
-    def calc_global_path(self, speed, c_d, c_d_d, c_d_dd, s0):
-        path = 0
+        # cost weights
+        K_J = 0.1
+        K_T = 0.1
+        K_D = 1.0
+        K_LAT = 1.0
+        K_LON = 1.0
 
-        return path
+        frenet_paths = []
 
-    def calc_check_path(self, speed, c_d, c_d_d, c_d_dd, s0):
+        # generate path to each offset goal
+        for di in np.arange(-MAX_ROAD_WIDTH, MAX_ROAD_WIDTH, D_ROAD_W):
+
+            # Lateral motion planning
+            for Ti in np.arange(MIN_T, MAX_T, DT):
+                fp = FrenetPath()
+
+                # lat_qp = quintic_polynomial(c_d, c_d_d, c_d_dd, di, 0.0, 0.0, Ti)
+                lat_qp = QuinticPolynomial(c_d, c_d_d, c_d_dd, di, 0.0, 0.0, Ti)
+
+                fp.t = [t for t in np.arange(0.0, Ti, DT)]
+                fp.d = [lat_qp.calc_point(t) for t in fp.t]
+                fp.d_d = [lat_qp.calc_first_derivative(t) for t in fp.t]
+                fp.d_dd = [lat_qp.calc_second_derivative(t) for t in fp.t]
+                fp.d_ddd = [lat_qp.calc_third_derivative(t) for t in fp.t]
+
+                # Longitudinal motion planning (Velocity keeping)
+                for tv in np.arange(TARGET_SPEED - D_T_S * N_S_SAMPLE,
+                                    TARGET_SPEED + D_T_S * N_S_SAMPLE, D_T_S):
+                    tfp = copy.deepcopy(fp)
+                    lon_qp = QuarticPolynomial(s0, c_speed, 0.0, tv, 0.0, Ti)
+
+                    tfp.s = [lon_qp.calc_point(t) for t in fp.t]
+                    tfp.s_d = [lon_qp.calc_first_derivative(t) for t in fp.t]
+                    tfp.s_dd = [lon_qp.calc_second_derivative(t) for t in fp.t]
+                    tfp.s_ddd = [lon_qp.calc_third_derivative(t) for t in fp.t]
+
+                    Jp = sum(np.power(tfp.d_ddd, 2))  # square of jerk
+                    Js = sum(np.power(tfp.s_ddd, 2))  # square of jerk
+
+                    # square of diff from target speed
+                    ds = (TARGET_SPEED - tfp.s_d[-1]) ** 2
+
+                    tfp.cd = K_J * Jp + K_T * Ti + K_D * tfp.d[-1] ** 2
+                    tfp.cv = K_J * Js + K_T * Ti + K_D * ds
+                    tfp.cf = K_LAT * tfp.cd + K_LON * tfp.cv
+
+                    frenet_paths.append(tfp)
+
+        return frenet_paths
+
+    def calc_global_paths(self, fplist, csp):
+        for fp in fplist:
+
+            # calc global positions
+            for i in range(len(fp.s)):
+                ix, iy = csp.calc_position(fp.s[i])
+                if ix is None:
+                    break
+                i_yaw = csp.calc_yaw(fp.s[i])
+                di = fp.d[i]
+                fx = ix + di * math.cos(i_yaw + math.pi / 2.0)
+                fy = iy + di * math.sin(i_yaw + math.pi / 2.0)
+                fp.x.append(fx)
+                fp.y.append(fy)
+
+            # calc yaw and ds
+            for i in range(len(fp.x) - 1):
+                dx = fp.x[i + 1] - fp.x[i]
+                dy = fp.y[i + 1] - fp.y[i]
+                fp.yaw.append(math.atan2(dy, dx))
+                fp.ds.append(math.hypot(dx, dy))
+
+            fp.yaw.append(fp.yaw[-1])
+            fp.ds.append(fp.ds[-1])
+
+            # calc curvature
+            for i in range(len(fp.yaw) - 1):
+                fp.c.append((fp.yaw[i + 1] - fp.yaw[i]) / fp.ds[i])
+
+        return fplist
+
+    def check_paths(self, speed, c_d, c_d_d, c_d_dd, s0):
         path = 0
 
         return path
@@ -309,7 +498,7 @@ class FrenetPlaner:
     def path_planner(self, vehicle_state, waypoints, timestep):
 
         #Calculate the optimal path in the frenet frame
-        fplist = self.calc_frenet_paths(vehicle_state[3], self.c_d, self.c_d_d, self.c_d_dd, s0)
+        fplist = self.calc_frenet_paths(vehicle_state[3], self.c_d, self.c_d_d, self.c_d_dd, self.s0)
 
         # Calculate the global path based on the waypoints
         fplist = self.calc_global_paths(fplist, csp)
